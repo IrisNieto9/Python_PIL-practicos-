15d3963c4b8fb4068af86bf1d8b6103f97d3e715
IrisNieto9 <iris.nieto.montoya@mi.unc.edu.ar>
2022-10-09 13:23:10 -0300

    Variables

---
+++ Clase_1/variables.py
@@ -0,0 +1,18 @@
+# Variables
+
+mensaje_usuario = input('Ingrese un mensaje: ')
+print(mensaje_usuario)
+print(mensajeUsuario)#Otra forma de escribir
+print('--------------------------------------')
+
+x = int(input('Ingrese un numero: '))
+y = int(input('Ingrese otro numero: '))
+
+print('El tipo de dato de X es: ',type(x))
+print('El tipo de dato de y es: ',type(y))
+
+
+
+resultado = x * y
+print('El resultado es --> ', resultado)
+
